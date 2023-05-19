45d5c428e63de0b147aab16be45784054eed1a19
katarzynach <kasia97h@o2.pl>
2023-05-19 13:36:50 +0200

    increment array

--- src/main/java/ArrayExercise.java
+++ src/main/java/ArrayExercise.java
@@ -1,13 +1,17 @@
 public class ArrayExercise {
 
-  public void arrayToUpperCase(String[] input) {
-    for(int i = 0; i < input.length; i++) {
-      input[i] = input[i].toUpperCase();
+    public void arrayToUpperCase(String[] input) {
+        for (int i = 0; i < input.length; i++) {
+            input[i] = input[i].toUpperCase();
+        }
     }
-  }
 
-  public void incrementArray(int[] input) {
-    // Add code here to increment each element in the array by 1
-  }
+    public void incrementArray(int[] input) {
+        // Add code here to increment each element in the array by 1
+        for (int i = 0; i < input.length; i++) {
+            input[i]=input[i]+1;
+            //input[i]++;
+        }
+    }
 
 }
--- src/test/java/ArrayExerciseTest.java
+++ src/test/java/ArrayExerciseTest.java
@@ -4,20 +4,27 @@ import org.junit.jupiter.api.Test;
 
 class ArrayExerciseTest {
 
-  ArrayExercise arrayExercise = new ArrayExercise();
+    ArrayExercise arrayExercise = new ArrayExercise();
 
-  @Test
-  public void testArrayToUpperCase() {
-    String[] input = new String[]{"cat", "dog", "fish"};
-    arrayExercise.arrayToUpperCase(input);
-    assertArrayEquals(new String[]{"CAT", "DOG", "FISH"}, input);
-  }
+    @Test
+    public void testArrayToUpperCase() {
+        String[] input = new String[]{"cat", "dog", "fish"};
+        arrayExercise.arrayToUpperCase(input);
+        assertArrayEquals(new String[]{"CAT", "DOG", "FISH"}, input);
+    }
 
-  @Test
-  public void testIncrementArray() {
-    int[] input = new int[]{1, 2, 3, 4, 5};
-    arrayExercise.incrementArray(input);
-    assertArrayEquals(new int[]{2, 3, 4, 5, 6}, input);
-  }
+    @Test
+    public void testIncrementArray() {
+        int[] input = new int[]{1, 2, 3, 4, 5};
+        arrayExercise.incrementArray(input);
+        assertArrayEquals(new int[]{2, 3, 4, 5, 6}, input);
+    }
+
+    @Test
+    public void testIncrementArray2() {
+        int[] input = new int[]{6, 4, 2, 3, 8};
+        arrayExercise.incrementArray(input);
+        assertArrayEquals(new int[]{7, 5, 3, 4, 9}, input);
+    }
 
 }
